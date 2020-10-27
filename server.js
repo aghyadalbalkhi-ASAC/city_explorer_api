3bbc57d0de7f64076d5b92db2afb126983c0c17a
M Aghead Albakhe <aghyadalbalkhi@gmail.com>
2020-10-28 00:22:11 +0300

    fixing heroku deployment prpblem

--- server.js
+++ server.js
@@ -33,15 +33,17 @@ app.get('/trails', handelTrails);
 
 
 function checklocation(req, res) {
+  console.log('checklocation');
   let city = req.query.city;
   let statment = `SELECT search_query,formatted_query,latitude,longitude FROM locations WHERE search_query='${city}';`;
   client.query(statment).then(data => {
     if (data.rowCount !== 0) {
+      console.log('data');
       res.send(data.rows[0]);
     }
 
     else {
-
+    console.log('handelLocation');
       handelLocation(city, req, res);
     }
 
@@ -60,24 +62,30 @@ function handelLocation(city, req, res) {
   currentcity = city;
   //Get the KEY Value From Env File
   const KEY = process.env.KEY;
-
+  console.log('handelLocation');
   // use the superagent for API Request (The URL Requested).then(arrow funcation (callback) which recive the data back from api)
   superagent.get(`https://eu1.locationiq.com/v1/search.php?key=${KEY}&q=${city}&format=json`)
     .then(data => {
-
+      console.log('superagent');
       //Store the data that back from API server  // Body where the reviced data stored
       let josnObject = data.body[0];
       let locationObject = new Location(city, josnObject.display_name, josnObject.lat, josnObject.lon);
       currentcitylat = josnObject.lat;
       currentcitylon = josnObject.lon;
 
-      let inserStetment = `INSERT INTO locations (search_query , formatted_query , latitude , longitude) VALUES ('${locationObject.search_query}','${locationObject.formatted_query}','${josnObject.lat}','${josnObject.lon}');`;
+      let inserStetment = `INSERT INTO locations (search_query , formatted_query , latitude , longitude) VALUES ($1,$2,$3,$4) RETURNING *;`;
+      let values = [city,josnObject.display_name, josnObject.lat, josnObject.lon];
+      console.log('inserStetment');
+      client.query(inserStetment,values).then(insertedRecord =>{
+        // For Hanaa res.send(insertedRecord.rows);
+        console.log('then');
+      }).catch( err =>{
+        console.log('catch');
+        // res.status(500).json({
 
-      client.query(inserStetment).then().catch( err =>{
-        res.status(500).json({
-          status: 500,
-          responseText: 'Sorry, something went wrong',
-        });
+        //   status: 500,
+        //   responseText: 'Sorry, something went wrong',
+        // });
       }
 
       );
